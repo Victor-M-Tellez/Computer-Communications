9cabc5fa7b66e0dca33b7fbede33526f483b161b
Victor Tellez <Victor-M-Tellez@users.noreply.github.com>
2017-03-03 06:37:39 -0500

    Update 001_Binomial.m

--- Random_Signals-Systems/001_Binomial.m
+++ Random_Signals-Systems/001_Binomial.m
@@ -1,23 +1,31 @@
-%binornd(..)
 clear
 close all
 clc
-NSize = 10000;
-p1 = .7;
-NBins = 10;   %NBINS independent trials. 
+p = .7;          % probability of success for each trial 0 ≤ p ≤ 1
+NTrials = 10;    % the number of trials
+NSize = 10000;   % The number of random numbers from the binomial 
+                 % distribution with parameters NTrials and P that we want
 
-rndArray = binornd( NBins , p1 , NSize, 1);
-histogram(rndArray, NBins);%---------------------------------------------Plot a histogram
-title('Samples from Binomial Distribution', 'Color', [.27 .89 .76])
 
+% R = binornd(N,P,[m,n])generates an m-by-n array containing random numbers 
+% from the binomial distribution with parameters N and P. 
+rndArray = binornd( NTrials , p ,[ NSize, 1]);
+histogram(rndArray, NTrials, 'FaceColor','g');%Plot a histogram
+title('\fontsize{20}  Samples from Binomial Distribution', 'Color', [0 0 0])
+
+%Experimental
+%returns the mean of the elements of A along the first array dimension whose size does not equal 1
 MeanData = mean(rndArray);% sample mean
+%returns the variance of the elements of A along the first array dimension whose size does not equal 1.
 VarData = var(rndArray);% sample variance
 
 %Theoretical 
-[mu, var] = binostat(NSize, p1);
-param = binofit(rndArray, NBins);
+% [M,V] = binostat(N,P) returns the mean of and variance for the binomial distribution 
+[M, V] = binostat(NSize, p);% sample mean & variance
 
-
-
-
-
+%Binomial parameter estimates
+% phat = binofit(x,n) returns a maximum likelihood estimate of the 
+% probability of success in a given binomial trial based on
+%       x==rndArray -->> the number of successes; observed in
+%       n==NTrials  -->> independent trials. 
+param = binofit(rndArray, NTrials);
